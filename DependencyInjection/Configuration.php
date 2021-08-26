3ec13f9647ea4e41afb7787291c2eb4b3db77ec6
Valentin ANDRE <valentin.andre@sfam.eu>
2021-08-26 17:59:46 +0200

    fix allow symfony 5 compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Johnstyle\RedmineBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Class Configuration
@@ -18,8 +19,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('johnstyle_redmine');
+        if (Kernel::VERSION_ID > 40000) {
+            $treeBuilder = new TreeBuilder('johnstyle_redmine');
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('johnstyle_redmine');
+        }
+
         $rootNode
             ->children()
                 ->scalarNode('url')->isRequired()->end()
